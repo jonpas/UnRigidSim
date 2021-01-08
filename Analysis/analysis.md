8cbc2ad438f1fbd6d2ab281841d0ce42c3c4b9e0
jonpas <jonpas33@gmail.com>
2021-01-08 14:19:31 +0100

    Measure FPS and plot data

--- Analysis/analysis.md
+++ Analysis/analysis.md
@@ -6,5 +6,11 @@ title: |
     Unreal Engine 4
 ---
 
-```{.matplotlib file=plot.py caption="Amount of objects vs FPS"}
+```{.matplotlib file=plot.py caption="Amount of objects vs FPS (mode, min, max)"}
 ```
+
+\vspace{1cm}
+
+Figure 1 shows that spheres are easier on physics simulation than cubes. At 5000 objects we assume we are reaching other bottlenecks, such as rendering that many objects.
+
+_All measurements were performed in the same scene, at the same camera location and rotation. Unreal Engine's FPS display was used._
--- Analysis/plot.py
+++ Analysis/plot.py
@@ -4,17 +4,29 @@ import sys
 import matplotlib.pyplot as plt
 
 # Object amount
-objects = [10, 100, 500, 1000, 5000]
+objects = [0, 10, 100, 500, 1000, 5000]
 
-# FPS
-cubes = [100, 60, 30, 20, 10]
-spheres = [90, 50,  25, 15, 8]
-combined = [95, 55, 28, 19, 9]
+# FPS [mean, low error, high error]
+cubes = [
+    [119, 119, 100, 35, 10, 6],
+    [1,     2,  10, 15,  2, 2],
+    [1,     1,  10,  5,  8, 1]
+]
+spheres = [
+    [119, 119, 110, 95, 55, 10],
+    [1,     2,   4,  5,  5,  2],
+    [1,     1,   8,  5,  5,  5]
+]
+combined = [
+    [119, 119, 115, 40, 12, 8],
+    [1,     2,  10,  5,  2, 4],
+    [1,     1,   3,  8,  4, 4]
+]
 
 plt.figure()
-plt.plot(objects, cubes, label="Cubes")
-plt.plot(objects, spheres, label="Spheres")
-plt.plot(objects, combined, label="Combined")
+plt.errorbar(objects, cubes[0], yerr=[cubes[1], cubes[2]], label="Cubes", capsize=5.0)
+plt.errorbar(objects, spheres[0], yerr=[spheres[1], spheres[2]], label="Spheres", capsize=5.0)
+plt.errorbar(objects, combined[0], yerr=[combined[1], combined[2]], label="Combined 50/50", capsize=5.0)
 plt.xlabel("Objects")
 plt.ylabel("FPS")
 plt.legend()
